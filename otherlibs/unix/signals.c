414909a1a77dae40ec6832fe379c7792c7106eaa
Jevin Sweval <jevinsweval@gmail.com>
2025-01-23 23:56:35 -0500

    cherry pick new signal.c from trunk for sigwait for threads fix

--- otherlibs/unix/signals.c
+++ otherlibs/unix/signals.c
@@ -13,12 +13,6 @@
 /*                                                                        */
 /**************************************************************************/
 
-/* CR ocaml 5 domains: This file is the 4.x version together with
-   adjustments to the names of exported functions ("unix_" -> "caml_unix").
-   (mshinwell/xclerc).
-   For multi-domain support we'll need to revisit this.
-*/
-
 #define CAML_INTERNALS
 
 #include <errno.h>
@@ -31,40 +25,32 @@
 #include <caml/signals.h>
 #include "caml/unixsupport.h"
 
-#ifndef NSIG
-#define NSIG 64
-#endif
-
 #ifdef POSIX_SIGNALS
 
 static void decode_sigset(value vset, sigset_t * set)
 {
   sigemptyset(set);
-  while (vset != Val_int(0)) {
+  for (/*nothing*/; vset != Val_emptylist; vset = Field(vset, 1)) {
     int sig = caml_convert_signal_number(Int_val(Field(vset, 0)));
     sigaddset(set, sig);
-    vset = Field(vset, 1);
   }
 }
 
 static value encode_sigset(sigset_t * set)
 {
-  value res = Val_int(0);
-  int i;
-
-  Begin_root(res)
-    for (i = 1; i < NSIG; i++)
-      if (sigismember(set, i) > 0) {
-        value newcons = caml_alloc_small(2, 0);
-        Field(newcons, 0) = Val_int(caml_rev_convert_signal_number(i));
-        Field(newcons, 1) = res;
-        res = newcons;
-      }
-  End_roots();
-  return res;
+  CAMLparam0();
+  CAMLlocal1(res);
+  for (int i = 1; i < NSIG; i++)
+    if (sigismember(set, i) > 0) {
+      value newcons = caml_alloc_2(Tag_cons,
+        Val_int(caml_rev_convert_signal_number(i)),
+        res);
+      res = newcons;
+    }
+  CAMLreturn(res);
 }
 
-static int sigprocmask_cmd[3] = { SIG_SETMASK, SIG_BLOCK, SIG_UNBLOCK };
+static const int sigprocmask_cmd[3] = { SIG_SETMASK, SIG_BLOCK, SIG_UNBLOCK };
 
 CAMLprim value caml_unix_sigprocmask(value vaction, value vset)
 {
@@ -75,11 +61,7 @@ CAMLprim value caml_unix_sigprocmask(value vaction, value vset)
   how = sigprocmask_cmd[Int_val(vaction)];
   decode_sigset(vset, &set);
   caml_enter_blocking_section();
-#ifdef CAML_RUNTIME_5
   retcode = pthread_sigmask(how, &set, &oldset);
-#else
-  retcode = caml_sigmask_hook(how, &set, &oldset);
-#endif
   caml_leave_blocking_section();
   /* Run any handlers for just-unmasked pending signals */
   caml_process_pending_actions();
@@ -90,11 +72,16 @@ CAMLprim value caml_unix_sigprocmask(value vaction, value vset)
 CAMLprim value caml_unix_sigpending(value unit)
 {
   sigset_t pending;
-  int i;
+  uintnat curr;
   if (sigpending(&pending) == -1) caml_uerror("sigpending", Nothing);
-  for (i = 1; i < NSIG; i++)
-    if(caml_pending_signals[i])
-      sigaddset(&pending, i);
+  for (int i = 0; i < NSIG_WORDS; i++) {
+    curr = atomic_load(&caml_pending_signals[i]);
+    if (curr == 0) continue;
+    for (int j = 0; j < BITS_PER_WORD; j++) {
+      if (curr & ((uintnat)1 << j))
+      sigaddset(&pending, i * BITS_PER_WORD + j + 1);
+    }
+  }
   return encode_sigset(&pending);
 }
 
@@ -110,6 +97,19 @@ CAMLprim value caml_unix_sigsuspend(value vset)
   return Val_unit;
 }
 
+CAMLprim value caml_unix_sigwait(value sigs)
+{
+  sigset_t set;
+  int retcode, signo;
+
+  decode_sigset(sigs, &set);
+  caml_enter_blocking_section();
+  retcode = sigwait(&set, &signo);
+  caml_leave_blocking_section();
+  if (retcode != 0) caml_unix_error(retcode, "sigwait", Nothing);
+  return Val_int(caml_rev_convert_signal_number(signo));
+}
+
 #else
 
 CAMLprim value caml_unix_sigprocmask(value vaction, value vset)
@@ -120,5 +120,8 @@ CAMLprim value caml_unix_sigpending(value unit)
 
 CAMLprim value caml_unix_sigsuspend(value vset)
 { caml_invalid_argument("Unix.sigsuspend not available"); }
+
+CAMLprim value caml_unix_sigwait(value vset)
+{ caml_invalid_argument("Unix.sigwait not available"); }
 
 #endif
